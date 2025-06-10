5fef3776048b70ec71c2171ce816d31151d05a23
vansh-cryp <vanshgusain920@gmail.com>
2025-06-10 22:35:35 +0530

    Movie Card Created

--- Movie Website/src/App.jsx
+++ Movie Website/src/App.jsx
@@ -1,9 +1,11 @@
+import MovieCard from './components/MovieCard';
 import './App.css'
-
 function App() {
 
   return (<div>
-    
+    <MovieCard movie={{title :"Bloodshot",release_year:"2024",url:""}}/>
+    <MovieCard movie={{title :"Bloodshot",release_year:"2024",url:""}}/>
+    <MovieCard movie={{title :"Bloodshot",release_year:"2024",url:""}}/>
   </div>);
 }
 
---
+++ Movie Website/src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+function MovieCard(movie){
+
+    function onLike(){
+        alert("clicked");
+    }
+    return(<div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className="favorite-button" onClick={onLike}></button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_year}</p>
+        </div>
+        </div>);
+}
+
+export default MovieCard;
